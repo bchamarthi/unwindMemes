25b2a9d875b9848b96fff958f3ff7f96b0ae0697
bchamarthi <brchamarthi29@gmail.com>
2019-08-02 02:02:28 -0400

    Initial Commit

---
+++ memes/ImageCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCollectionViewCell.swift
+//  memes
+//
+//  Created by KC on 8/1/19.
+//  Copyright © 2019 KC. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgImage: UIImageView!
+    
+    
+    
+    
+    
+    
+}
--- memes/ViewController.swift
+++ memes/ViewController.swift
@@ -6,14 +6,32 @@
 //  Copyright © 2019 KC. All rights reserved.
 //
 
+
+
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    var imageArray = [UIImage(named: "images-1"), UIImage(named: "images-2"), UIImage(named: "images"), UIImage(named: "Unknown-1"), UIImage(named: "Unknown-2"), UIImage(named: "Unknown-3")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCollectionViewCell", for: indexPath) as! ImageCollectionViewCell
+        
+        cell.imgImage.image = imageArray[indexPath.row]
+        
+        return cell
+    }
+
 
 
 }
